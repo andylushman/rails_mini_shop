746b4e3c4226381fe0d59f9236b23a35a919ebbb
Cole Worsley <cole.s.worsley@gmail.com>
2020-02-07 11:45:48 -0700

    update merchant item path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,7 @@ class ItemsController < ApplicationController
 
   # GET /items/1/edit
   def edit
+    binding.pry
   end
 
   # POST /items
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:show, :destroy]
   resources :merchants do
-    resources :items, only: [:index, :new, :create]
+    resources :items, only: [:index, :new, :create, :edit]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
