d50bce333bcb7bc106c3d4709adeeb48ed2ec579
Cole Worsley <cole.s.worsley@gmail.com>
2020-02-07 11:38:02 -0700

    WIP: setup routes for controllers

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,3 @@
 class Merchant < ApplicationRecord
-  has_many :rates
+  has_many :items, foreign_key: 'merchant_id'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
+  resources :items, only: [:show, :destroy]
   resources :merchants do
-    resources :items
+    resources :items, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
